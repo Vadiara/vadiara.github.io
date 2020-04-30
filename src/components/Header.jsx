53527a741de6a595179d8f9f8c7f4e5d952ebb5f
Vadiara <fest>
2020-04-29 23:15:37 -0500

    Delete old files and move my new project to GitHub

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header () {
+    return (
+        <header className='header'>
+        <img src='https://logomaster.ai/static/media/gallery002.936afb9d.png' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Profile () {
+    return (
+        <div className='content'>
+        <img src='https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'/>
+        <div>
+        ava+discr
+      </div>
+      <div>
+        My posts
+      </div>
+      <div>
+        New posts
+      </div>
+      <div>
+        post 1
+      </div>
+      </div>
+    );
+}
+
+export default Profile;
